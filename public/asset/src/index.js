5c44ab29adec7db47535a5b27aa7480b6c2dbd00
manish sharma <sharma001manish@gmail.com>
2020-03-29 18:22:57 +0530

    sorted

--- public/asset/src/index.js
+++ public/asset/src/index.js
@@ -1,1 +1,12 @@
-console.log("js loaded")
+
+$(document).ready(function(){
+
+   $(".nav-btn").click(function(){
+       let id = this.id;
+        $('html, body').animate({
+            scrollTop: ($("#"+id+"-div").offset().top)
+        },1000);
+
+   })
+  
+  });
